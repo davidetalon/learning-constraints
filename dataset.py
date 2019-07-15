backprop fixed: generative network propagates

dataset.py
@@ -50,15 +50,15 @@ class CSPDataset(Dataset):
 
         print('rnd assgn', random_assignments.shape, consistency.shape, rnd_assignments.shape)
         self.assignments = np.concatenate((sat_assignments, rnd_assignments), 0)
-
+        
 
         # add random noise to assignments
-        random_increments = np.random.randint(-1, 1, size=self.assignments.shape)
-        random_increments[:, -1] = 0
+        # random_increments = np.random.randint(-1, 1, size=self.assignments.shape)
+        # random_increments[:, -1] = 0
 
-        self.assignments = np.add(self.assignments, random_increments)
-        consistency = matrix_assignment_consistency(torch.from_numpy(self.assignments[:, :-1]), torch.from_numpy(self.csp.matrix), int(self.csp.d), self.csp.n)
-        self.assignments[:, -1] = consistency
+        # self.assignments = np.add(self.assignments, random_increments)
+        # consistency = matrix_assignment_consistency(torch.from_numpy(self.assignments[:, :-1]), torch.from_numpy(self.csp.matrix), int(self.csp.d), self.csp.n)
+        # self.assignments[:, -1] = consistency
 
     def __len__(self):
         """

model.py
@@ -13,6 +13,14 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
+def printgradnorm(self, grad_input, grad_output):
+    print('Inside ' + self.__class__.__name__ + ' backward')
+    print('Inside class:' + self.__class__.__name__)
+    print('')
+    print('grad_input size:', grad_input[0].size())
+    print('grad_output size:', grad_output[0].size())
+    print('grad_input norm:', grad_input[0].norm())
+
 class Generator(nn.Module):
 
     def __init__(self, input_size, csp_shape, dropout_prob= 0):
@@ -24,7 +32,6 @@ class Generator(nn.Module):
         self.sat_matrix_size = csp_shape['n']*csp_shape['d']
 
         self.conv = nn.Sequential(
-            nn.BatchNorm2d(1),
             nn.ConvTranspose2d( 1, out_channels=8, kernel_size=3, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(8),
             nn.ReLU(),
@@ -37,22 +44,23 @@ class Generator(nn.Module):
             nn.ConvTranspose2d( 32, out_channels=64, kernel_size=3, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.ConvTranspose2d( 64, out_channels=128, kernel_size=3, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU()
+            # nn.ConvTranspose2d( 64, out_channels=128, kernel_size=3, stride=1, padding=0, bias=False),
+            # nn.BatchNorm2d(128),
+            # nn.ReLU()
         )
 
         self.dense = nn.Sequential(
-            nn.Linear(2437632, 512),
+            nn.Linear(1183744, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, self.sat_matrix_size*self.sat_matrix_size + csp_shape['n']*self.d + 1)
+            nn.Linear(512, self.sat_matrix_size*self.sat_matrix_size + csp_shape['n'] + 1)
         )
 
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Softmax(dim=-1)
+        self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
+
+
 
 
 
@@ -64,32 +72,26 @@ class Generator(nn.Module):
 
         x = self.dense(x)
 
-        print(x.shape)
         matrix = torch.narrow(x, 1, 0, self.sat_matrix_size*self.sat_matrix_size)
-        print(matrix.shape)
         matrix = matrix.view(matrix.size()[0], self.sat_matrix_size, -1)
-        matrix = self.tanh(x)
-        matrix = torch.where(matrix < 0.0, torch.zeros(matrix.shape), torch.ones(matrix.shape))
-        print('matrix', matrix.shape)
+        matrix = self.sigmoid(matrix)
+        matrix = torch.round(matrix)
+        # matrix = torch.where(matrix < 0.5, torch.zeros(matrix.shape, requires_grad=True), torch.ones(matrix.shape, requires_grad=True))
 
-        assignments = torch.narrow(x, 1, self.sat_matrix_size * self.sat_matrix_size, self.n * self.d)
-        assignments = assignments.view(assignments.size()[0], -1, self.n,)
-        assignments = self.softmax(assignments)
-        assignments = torch.argmax(assignments, dim=1, keepdim=True)
-        print('assignment', assignments.shape)
+        assignments = torch.narrow(x, 1, self.sat_matrix_size * self.sat_matrix_size, self.n)
+        assignments = assignments.view(assignments.size()[0], -1, self.n)
+        assignments = self.relu(assignments)
+        assignments = torch.round(assignments)
 
-        sat_label = torch.narrow(x, 1, self.sat_matrix_size * self.sat_matrix_size + self.n * self.d, 1)
+        sat_label = torch.narrow(x, 1, self.sat_matrix_size * self.sat_matrix_size + self.n, 1)
         sat_label = sat_label.view(sat_label.size()[0], 1, -1)
         sat_label = self.sigmoid(sat_label)
-        sat_label = torch.where(sat_label < 0.5, torch.zeros(sat_label.shape), torch.ones(sat_label.shape))
+        sat_label = torch.round(sat_label)
+        # sat_label = torch.where(sat_label < 0.5, torch.zeros(sat_label.shape, requires_grad=True), torch.ones(sat_label.shape, requires_grad=True))
 
-        print('sat_labels', sat_label.shape)
+        labeled_assignments = torch.cat((assignments.type(torch.float), sat_label.type(torch.float)), dim=-1)
 
-        print('ass ', type(assignments), ' sat', type(sat_label))
-        assignments = torch.cat((assignments.type(torch.float), sat_label.type(torch.float)), dim=-1)
-
-        print('cat', assignments.shape)
-        return matrix, assignments
+        return matrix, labeled_assignments
 
 class Discriminator(nn.Module):
 
@@ -117,7 +119,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(),
             nn.Dropout(0.5),
             nn.Linear(32, 1),
-            nn.Sigmoid() 
+            nn.Sigmoid()
         )
 
     def forward(self, x):
@@ -153,8 +155,12 @@ def train_batch(gen, discr, batch, csp_size, loss_fn, optimizer):
     discr_optimizer.zero_grad()
 
     # labels
-    real = torch.full((batch.shape[0], 1),fill_value=0.9)
-    fake = torch.zeros(batch.shape[0], 1)
+    # real = torch.full((batch.shape[0], 1),fill_value=0.9)
+    # fake = torch.zeros(batch.shape[0], 1)
+
+    # with flipped labels and smoothing
+    real = torch.empty((batch.shape[0],1)).uniform_(0,0.1)
+    fake = torch.empty((batch.shape[0],1)).uniform_(0.9, 1)
 
     # computing the loss
     output = discr(batch)
@@ -165,47 +171,25 @@ def train_batch(gen, discr, batch, csp_size, loss_fn, optimizer):
     real_loss.backward()
 
     # generate the CSP
-    mean = torch.zeros(batch.shape[0], 1, 128, 128)
-    variance = torch.ones(batch.shape[0], 1, 128, 128)
-    rnd_assgn = torch.normal(mean, variance)
+    # mean = torch.zeros(batch.shape[0], 1, 128, 128)
+    # variance = torch.ones(batch.shape[0], 1, 128, 128)
+    # rnd_assgn = torch.normal(mean, variance, requires_grad=True)
+    rnd_assgn = torch.randn((batch.shape[0], 1, 128, 128))
 
     fake_csp, fake_batch = gen(rnd_assgn)
 
     # let's solve each problem with a solution
     n = int(csp_size['n'])
     d = int(csp_size['d'])
-
-    # assignment = np.empty((fake_csp.shape[0], n+1))
-    # for csp in range(fake_csp.shape[0]):
-    #     solver = CSPSolver(n, d, fake_csp[csp, :, :], limit=1)
-    #     if solver.solution_count()>=1:
-    #         assgn = solver.get_satisfying_assignments(n=1)
-    #         assgn = np.append(assgn, 1)
-
-    #     else:
-    #         assgn = np.random.randint(0, d-1, n)
-    #         assgn = np.append(assgn, 0)
-    #     assignment[csp, :] = assgn[:]
     
-    # TODO:add non-sat assignments
-    # random_assignment = self.csp.generate_rnd_assignment(size)
-
-    #     # add satisfying assingnments
-    #     if solv.solution_count()>0:
-    #         satisfying_assignments = solv.get_satisfying_assignments()
-    #         print(satisfying_assignments.shape, self.assignments.shape)
-    #         self.assignments = np.concatenate((self.assignments, satisfying_assignments), axis=0)
-    # assignment = ass
-    # fake_batch = torch.from_numpy(assignment)
-    # fake_batch.unsqueeze_(1)
-    # fake_batch = fake_batch.type(torch.float)
-    
-    print(fake_batch.shape)
     output = discr(fake_batch.detach())
     fake_loss = loss_fn(output, fake)
 
     fake_loss.backward()
     D_G_z1 = output.mean().item()
+
+    discr_top = discr.model[0].weight.grad
+    discr_bottom = discr.dense[-2].weight.grad
 
     discr_loss = (real_loss + fake_loss)/2
 
@@ -220,12 +204,18 @@ def train_batch(gen, discr, batch, csp_size, loss_fn, optimizer):
     output = discr(fake_batch)
     gen_loss = loss_fn(output, real)
 
+    
+
     D_G_z2 = output.mean().item()
     gen_loss.backward()
 
+    gen_top = gen.conv[0].weight.grad
+    gen_bottom = gen.dense[-1].weight.grad
+
+
     gen_optimizer.step()
 
-    return gen_loss, discr_loss, D_x, D_G_z1, D_G_z2
+    return gen_loss, discr_loss, D_x, D_G_z1, D_G_z2, discr_top, discr_bottom, gen_top, gen_bottom
 
 
 from csp import CSP

train.py
@@ -2,7 +2,7 @@ import argparse
 import torch
 from dataset import ToTensor, CSPDataset
 from torch.utils.data import DataLoader, Dataset
-from model import Generator, Discriminator, train_batch, weights_init
+from model import Generator, Discriminator, train_batch, weights_init, printgradnorm
 import json
 from pathlib import Path
 import time
@@ -70,6 +70,12 @@ if __name__ == '__main__':
     d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=args.discr_lr, betas=(0.5, 0.999))
     optimizer = {'gen': g_optimizer, 'discr':d_optimizer}
 
+    # hooks to print gradients
+    # discriminator.model.register_forward_hook(printgradnorm)
+    # discriminator.dense.register_forward_hook(printgradnorm)
+    # generator.conv.register_forward_hook(printgradnorm)
+    # generator.dense.register_forward_hook(printgradnorm)
+
 
     print("Start training")
     gen_loss_history = []
@@ -77,6 +83,12 @@ if __name__ == '__main__':
     D_x_history = []
     D_G_z1_history = []
     D_G_z2_history = []
+
+    discr_top_grad =[]
+    discr_bottom_grad=[]
+    gen_top_grad = []
+    gen_bottom_grad = []
+
     for epoch in range(args.num_epochs):
 
         # Iterate batches
@@ -88,15 +100,21 @@ if __name__ == '__main__':
 
             # Update network
             start = time.time()
-            gen_loss, discr_loss, D_x, D_G_z1, D_G_z2= train_batch(generator, discriminator, batch, \
+            gen_loss, discr_loss, D_x, D_G_z1, D_G_z2, discr_top, discr_bottom, gen_top, gen_bottom = train_batch(generator, discriminator, batch, \
                 csp_shape, adversarial_loss, optimizer)
 
+            print(discr_top.shape)
+            print(torch.sum(gen_bottom).item())
             # saving metrics
             gen_loss_history.append(gen_loss.item())
             discr_loss_history.append(discr_loss.item())
             D_x_history.append(D_x)
             D_G_z1_history.append(D_G_z1)
             D_G_z2_history.append(D_G_z1)
+            discr_top_grad.append(torch.sum(discr_top).item())
+            discr_bottom_grad.append(torch.sum(discr_bottom).item())
+            gen_top_grad.append(torch.sum(gen_top).item())
+            gen_bottom_grad.append(torch.sum(gen_bottom).item())
 
             end = time.time()
             print("[Time %d s][Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f] [D(x): %f, D(G(Z)): %f / %f]"
@@ -116,6 +134,8 @@ if __name__ == '__main__':
 
         csp, assgn = generator(rnd_assgn)
     
+    print('csp', dataset.csp.matrix.shape)
+    print('fake csp', csp.shape)
     errors = torch.sum((torch.from_numpy(dataset.csp.matrix).type(torch.float) - csp).pow(2)).item()
 
     matrix_size = csp_shape['n']*csp_shape['d']
@@ -146,7 +166,9 @@ if __name__ == '__main__':
     with open(out_dir / csp_file_name, 'w') as f:
         json.dump(dataset.csp.matrix.tolist(), f, indent=4)
     
-    metrics = {'n_sat_assignments':dataset.n_sat_assignments, 'acc':accuracy, 'gen_loss':gen_loss_history, 'discr_loss':discr_loss_history, 'D_x':D_x_history, 'D_G_z1':D_G_z1_history, 'D_G_z2':D_G_z2_history}
+    metrics = {'n_sat_assignments':dataset.n_sat_assignments, 'acc':accuracy, 'gen_loss':gen_loss_history, \
+        'discr_loss':discr_loss_history, 'D_x':D_x_history, 'D_G_z1':D_G_z1_history, 'D_G_z2':D_G_z2_history, \
+        'gen_top':gen_top_grad, 'gen_bottom':gen_bottom_grad, 'discr_top':discr_top_grad, 'discr_bottom':discr_bottom_grad}
     
     # Save metrics
     metric_file_name = 'metrics'+ date +'.json'